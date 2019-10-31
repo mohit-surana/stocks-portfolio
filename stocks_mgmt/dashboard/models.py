9c422ee6b3ec41aa52883436bfa18e1a440ae5ec
mohit-surana <mohit.surana95@gmail.com>
2019-10-31 13:56:25 -0700

    Initial data model for single user system

--- stocks_mgmt/dashboard/models.py
+++ stocks_mgmt/dashboard/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+class Ticker(models.Model):
+    symbol = models.CharField(max_length=8, primary_key=True)
+    name = models.CharField(max_length=64)
+
+
+class Watchlist(models.Model):
+    symbol = models.OneToOneField(Ticker, on_delete=models.CASCADE)
+    time_added = models.DateTimeField(auto_now=True)
+
+
+class Portfolio(models.Model):
+    symbol = models.OneToOneField(Ticker, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    time_added = models.DateTimeField(auto_now=True)
+
+
+class HistoricalStockPrice(models.Model):
+    symbol = models.ForeignKey(Ticker, on_delete=models.CASCADE)
+    date = models.DateField()
+    open_price = models.DecimalField(decimal_places=2, max_digits=16)
+    close_price = models.DecimalField(decimal_places=2, max_digits=16)
+    high = models.DecimalField(decimal_places=2, max_digits=16)
+    low = models.DecimalField(decimal_places=2, max_digits=16)
+    volume = models.IntegerField()
+    time_added = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = (('symbol', 'date'),)
+
+
+class IntradayStockPrice(models.Model):
+    symbol = models.ForeignKey(Ticker, on_delete=models.CASCADE)
+    time = models.DateTimeField()
+    open_price = models.DecimalField(decimal_places=2, max_digits=16)
+    close_price = models.DecimalField(decimal_places=2, max_digits=16)
+    high = models.DecimalField(decimal_places=2, max_digits=16)
+    low = models.DecimalField(decimal_places=2, max_digits=16)
+    volume = models.IntegerField()
+    time_added = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = (('symbol', 'time'),)
